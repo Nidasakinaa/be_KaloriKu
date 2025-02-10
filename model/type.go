fb1e0b54453fee9c3d9d5612b6e944d8a7fb2b5f
nida sakinah aulia <nidasakinaaulia@gmail.com>
2025-02-10 18:55:30 +0700

    refactor: reorganize User and Token structs for improved clarity and consistency

--- model/type.go
+++ model/type.go
@@ -1,8 +1,9 @@
 package model
 
 import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type MenuItem struct {
@@ -11,23 +12,23 @@ type MenuItem struct {
 	Ingredients string             `bson:"ingredients,omitempty" json:"ingredients,omitempty"`
 	Description string             `bson:"description,omitempty" json:"description,omitempty"`
 	Calories    float64            `bson:"calories,omitempty" json:"calories,omitempty"`
-	Category    string             `bson:"category,omitempty" json:"category,omitempty"` 
+	Category    string             `bson:"category,omitempty" json:"category,omitempty"`
 	Image       string             `bson:"image,omitempty" json:"image,omitempty"`
 }
 
 type User struct {
-    ID       primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-    FullName string             `bson:"name,omitempty" json:"name,omitempty"`
-    Phone    string             `bson:"phone,omitempty" json:"phone,omitempty"`
-    Username string             `bson:"username,omitempty" json:"username,omitempty"`
-    Password string             `bson:"password,omitempty" json:"password,omitempty"`
-    Role     string             `bson:"role,omitempty" json:"role,omitempty"` // "admin" or "customer"
+	ID                     primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	FullName               string             `bson:"name,omitempty" json:"name,omitempty"`
+	Phone                  string             `bson:"phone,omitempty" json:"phone,omitempty"`
+	Username               string             `bson:"username,omitempty" json:"username,omitempty"`
+	Password               string             `bson:"password,omitempty" json:"password,omitempty"`
+	Role                   string             `bson:"role,omitempty" json:"role,omitempty"` // "admin" or "customer"
+	PersonalizedCategories []string           `bson:"personalized_categories,omitempty" json:"personalized_categories,omitempty"`
 }
 
-type Token struct{
-	ID			string 				`bson:"_id,omitempty" json:"_id,omitempty"`
-	Token		string				`bson:"token" json:"token,omitempty"`
-	AdminID		string				`bson:"admin_id" json:"admin_id,omitempty"`
-	CreatedAt	time.Time			`bson:"created_at" json:"created_at"` 
+type Token struct {
+	ID        string    `bson:"_id,omitempty" json:"_id,omitempty"`
+	Token     string    `bson:"token" json:"token,omitempty"`
+	AdminID   string    `bson:"admin_id" json:"admin_id,omitempty"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
 }
-
